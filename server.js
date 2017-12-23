8586e5ea291594fae97e32393a3385685e4843ea
Atle Magnussen <atlemagnussen@gmail.com>
2017-12-23 08:34:40 +0100

    modified server.js to work and removed some stuff including web folder

--- server.js
+++ server.js
@@ -1,28 +1,12 @@
-const config = require('./config.js');
-const logger = require('./logger.js').default;
-logger.debug('Booting');
-
 const path = require('path');
 const express = require('express');
 const http = require('http');
-const url = require('url');
-const ws = require('ws');
 
 // static server
 const app = express();
 var router = express.Router();
 app.use(express.static(path.join(__dirname, 'app')));
 const server = http.createServer(app);
-// web socket
-const wss = new ws.Server({ server });
-wss.on('connection', function connection(ws, req) {
-    const location = url.parse(req.url, true);
-    logger.trace("location: " + JSON.stringify(location));
-    ws.on('message', function incoming(message) {
-        console.log('received: %s', message);
-    });
-
-});
 
 // api
 router.get('/', function (req, res) {
@@ -30,10 +14,10 @@ router.get('/', function (req, res) {
 });
 
 router.get('/config', function (req, res) {
-    res.json(config);
+    res.json("hey");
 });
 
 app.use('/api', router);
-server.listen(config.port, function listening() {
-    logger.info('Listening on %d', server.address().port);
+server.listen(8888, function listening() {
+    console.log('Listening on %d', server.address().port);
 });
