844488fa600563d00f8a32ea731e50f9de63511f
Atle Magnussen <atlemagnussen@gmail.com>
2017-12-28 15:04:40 +0100

    started integrating facebook

---
+++ app/services/facebook.service.js
@@ -0,0 +1,28 @@
+angular.module('services', [])
+.factory('facebookService', function($q) {
+    return {
+        getMyLastName: function() {
+            var deferred = $q.defer();
+            FB.api('/me', {
+                fields: 'last_name'
+            }, function(response) {
+                if (!response || response.error) {
+                    deferred.reject('Error occured');
+                } else {
+                    deferred.resolve(response);
+                }
+            });
+            return deferred.promise;
+        },
+        checkLoginState: function() {
+            var deferred = $q.defer();
+            FB.getLoginStatus(function(response) {
+                if (!response || response.error) {
+                    deferred.reject('Error occured');
+                } else {
+                    deferred.resolve(response);
+                }
+            });
+        }
+    };
+});
